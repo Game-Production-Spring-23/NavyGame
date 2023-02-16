bb0330cdaceda1a81de8ccd02184e10fc1fc2949
Andy-of-Indiana <99208998+AndrewOfIndana@users.noreply.github.com>
2023-02-16 16:31:27 -0500

    Integrated 1 question and answer

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+//On click function for start test
+function startTest() {
+  getQuestion();
+}
+function getQuestion() {
+  //Fetches data from json file, can be used with api url
+  fetch("/data-example/data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      //store changes to HTML in app and appends changes to app div
+      let app = "";
+      app += "<h1>" + data.questions.question_1.prompt + "</h1>";
+
+      //Creates buttons for each answer, will also incorporate dials.
+      for (let i = 0; i < data.questions.question_1.answers.length; i++) {
+        app +=
+          "<h2><input class='answer' type='button' onclick='getAnswer(" +
+          i +
+          "," +
+          data.questions.question_1.correct_answer_index +
+          ")' value='" +
+          data.questions.question_1.answers[i] +
+          "'></h2>";
+      }
+      app += "<div id='response'></div>";
+
+      document.getElementById("app").innerHTML = app;
+    });
+}
+//On click function for clicking on a right or wrong answer
+function getAnswer(guess, correct) {
+  //if the user clicks the right button the app resets
+  if (guess == correct) {
+    document.getElementById("app").innerHTML =
+      "<h1>Correct</h1><input id='start' type='button' onclick='getQuestion()'' value='Redo Test'>";
+  } else {
+    //if the user clicks the wrong answer, the user is told that it is wrong
+    document.getElementById("response").innerHTML =
+      "<h3>Wrong answer try again</h3>";
+  }
+}
--- mediaTest/audio/audioTest.js
+++ mediaTest/audio/audioTest.js
@@ -1,9 +1,4 @@
 /* START */
-//Plays background music
-var music = new Audio("SFXmusic.mp3");
-music.volume = 0.4;
-music.play();
-
 //Sets up other sound effects
 const rightSfx = "SFXright.mp3";
 const wrongSfx = "SFXwrong.mp3";
@@ -11,6 +6,13 @@ var audio = new Audio(wrongSfx);
 
 //On click function for start test
 function startTest() {
+  //Plays background music
+  var music = new Audio("SFXmusic.mp3");
+  music.volume = 0.4;
+  music.play();
+  getQuestion();
+}
+function getQuestion() {
   //Fetches data from json file, can be used with api url
   fetch("questions.json")
     .then((response) => response.json())
@@ -22,8 +24,10 @@ function startTest() {
       //Creates buttons for each answer, will also incorporate dials.
       for (let i = 0; i < data.questions.question_1.answers.length; i++) {
         app +=
-          "<h2><input class='answer' type='button' onclick='answer(" +
+          "<h2><input class='answer' type='button' onclick='getAnswer(" +
           i +
+          "," +
+          data.questions.question_1.correct_answer_index +
           ")' value='" +
           data.questions.question_1.answers[i] +
           "'></h2>";
@@ -33,25 +37,21 @@ function startTest() {
       document.getElementById("app").innerHTML = app;
     });
 }
-
 //On click function for clicking on a right or wrong answer
-function answer(index) {
-  fetch("questions.json")
-    .then((response) => response.json())
-    .then((data) => {
-      //if the user clicks the right button the app resets
-      if (index == data.questions.question_1.correct_answer_index) {
-        audio = new Audio(rightSfx);
-        audio.volume = 0.4;
-        audio.play();
-        document.getElementById("app").innerHTML =
-          "<h1>Correct</h1><input id='start' type='button' onclick='startTest()'' value='Redo Test'>";
-      } else {
-        audio = new Audio(wrongSfx);
-        audio.volume = 0.4;
-        audio.play();
-        document.getElementById("response").innerHTML =
-          "<h3>Wrong answer try again</h3>";
-      }
-    });
+function getAnswer(guess, correct) {
+  //if the user clicks the right button the app resets
+  if (guess == correct) {
+    audio = new Audio(rightSfx);
+    audio.volume = 0.4;
+    audio.play();
+    document.getElementById("app").innerHTML =
+      "<h1>Correct</h1><input id='start' type='button' onclick='getQuestion()'' value='Redo Test'>";
+  } else {
+    //if the user clicks the wrong answer, the user is told that it is wrong
+    audio = new Audio(wrongSfx);
+    audio.volume = 0.4;
+    audio.play();
+    document.getElementById("response").innerHTML =
+      "<h3>Wrong answer try again</h3>";
+  }
 }
