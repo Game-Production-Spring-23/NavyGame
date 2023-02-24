1bff316ff0ecd199892d3f6f506b8f669dfa0015
Andy-of-Indiana <99208998+AndrewOfIndana@users.noreply.github.com>
2023-02-24 01:47:48 -0500

    Added splash screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,25 @@
 //global variables
+let splash = document.getElementById("splash");
 let bottomLinks = document.getElementById("bottomLinks"); //Links at bottom of start screen
 let gameContainer = document.getElementById("gameContainer"); //Container for game
 let settingsContainer = document.getElementById("settingsContainer"); //Container for settings
 let scoreContainer = document.getElementById("scoreContainer");
 let journalScreen = document.getElementById("journalContainer");
 let mapScreen = document.getElementById("mapContainer");
+let music = new Audio("/assets/audio/SFXmusic.mp3");
 
+scoreContainer.style.display = "none"; //Container for score
+settingsContainer.style.display = "none";
+journalScreen.style.display = "none";
+mapScreen.style.display = "none";
 
-scoreContainer.style.display="none";//Container for score
-settingsContainer.style.display="none";
-journalScreen.style.display="none";
-mapScreen.style.display="none";
-
- 
-
-
-
-
+splashScreen();
 
 //On click function for start test
 function startTest() {
-  bottomLinks.style.display="none";
-  scoreContainer.style.display="block";
+  bottomLinks.style.display = "none";
+  scoreContainer.style.display = "block";
   getQuestion();
-  
 }
 function getQuestion() {
   //Fetches data from json file, can be used with api url
@@ -57,136 +53,134 @@ function getAnswer(guess, correct) {
     //Currently cycles back to start of test, but will eventually load future questions.
     document.getElementById("gameContainer").innerHTML =
       "<h1>Correct</h1><input id='start' class='button-8' type='button' onclick='location.reload()' value='Redo Test'>";
-      updateScore();
+    updateScore();
   } else {
     //if the user clicks the wrong answer, the user is told that it is wrong
     document.getElementById("response").innerHTML =
       "<h3>Wrong answer try again</h3>";
-
   }
 }
 
 //Future Functions -------------------------------------------------//
 
 //grabs a video from the queue and plays it in the gameContainer div
-function playVideo(){
+function playVideo() {
   //grabs video from queue
   //plays video in gameContainer div
-
 }
 
-function updateScore(){ //convert innerhtml to int and add 1
+function updateScore() {
+  //convert innerhtml to int and add 1
   var score = document.getElementById("score");
   score.innerHTML = parseInt(score.innerHTML) + 1;
   console.log(score);
 }
 
 //Resets score to zero
-function clearScore(){
+function clearScore() {
   var score = document.getElementById("score");
   score.innerHTML = 0;
   console.log(score);
 }
 
 //grabs a dialogue from the queue and plays it in the gameContainer div
-function startDialogue(){
+function startDialogue() {
   //grabs dialogue from queue
   //plays dialogue in gameContainer div
-
 }
 
 //closes the dialogue and resumes the game
-function endDialogue(){
+function endDialogue() {
   //ends dialogue
   //updates queue number
 }
 
 //closes the video and resumes the game
-function endVideo(){
+function endVideo() {
   //ends video
   //updates queue number
 }
 
 //shows main settings screen--TESTED. Works.
-function showOptions(){
+function showOptions() {
   //shows options, hides bottom links and replaces elements with settingsContainer
-  bottomLinks.style.display="none";
-  gameContainer.style.display="none";
-  settingsContainer.style.display="block";
-  score.style.visibility="hidden";
-
-  
-  
+  bottomLinks.style.display = "none";
+  gameContainer.style.display = "none";
+  settingsContainer.style.display = "block";
+  score.style.visibility = "hidden";
 }
 
 //Tested, Works.
-function hideOptions(){
+function hideOptions() {
   //hides options
-  bottomLinks.style.visibility="visible";
-
+  bottomLinks.style.visibility = "visible";
 }
 
 //plays or shows the introduction
-function introduction(){
+function introduction() {
   //shows introduction
 }
 
 //shows map in a pop up box when clicked
-function toggleMapOn(){ //Tested, works
-  mapScreen.style.display="block";
-  
+function toggleMapOn() {
+  //Tested, works
+  mapScreen.style.display = "block";
 }
 
 //hides map popup box when clicked
-function toggleMapOff(){ //Tested, works
-  mapScreen.style.display="none";
+function toggleMapOff() {
+  //Tested, works
+  mapScreen.style.display = "none";
 }
 
-function toggleSettingsOn(){
+function toggleSettingsOn() {
   //shows settings in a pop up box when clicked
-
 }
 
-function toggleSettingsOff(){
+function toggleSettingsOff() {
   //hides settings popup box when clicked
 }
 
-function toggleJournalOn(){ //tested, works
+function toggleJournalOn() {
+  //tested, works
   //shows journal in a pop up box when clicked
-  journalScreen.style.display="block";
+  journalScreen.style.display = "block";
 }
 
 //hides journal popup box when clicked
-function toggleJournalOff(){ //tested, works
-  journalScreen.style.display="none";
+function toggleJournalOff() {
+  //tested, works
+  journalScreen.style.display = "none";
 }
 
-function volumeUp(){
+function volumeUp() {
   //increases volume
-
 }
 
-function volumeDown(){
+function volumeDown() {
   //decreases volume
-
-} 
-
-function splashScreen(){
-  //shows splash screen
-
 }
 
-function pause(){
+function splashScreen() {
+  splash.addEventListener("click", () => {
+    //Plays background music
+    music.volume = 0.4;
+    music.play();
+    splash.style.opacity = 0;
+    setTimeout(() => {
+      splash.classList.add("hidden");
+    }, 610);
+  });
+}
+
+function pause() {
   //pauses game
-
 }
 
-function resume(){
+function resume() {
   //resumes game
-
 }
 
-function continueDialogue(){
+function continueDialogue() {
   //continues dialogue
-
 }
