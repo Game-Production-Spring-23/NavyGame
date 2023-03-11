8ff444d29af90099364f74eda31a2929d914806f
Andy-of-Indiana <99208998+AndrewOfIndana@users.noreply.github.com>
2023-03-11 00:36:12 -0500

    Screen 3 Intergration

--- app/Screen3/screen3.js
+++ app/Screen3/screen3.js
@@ -1,124 +1,154 @@
-/*
-    Screen 1 Template
+import { Overlay } from "../core/classes/Overlay.js";
 
-    Tasks:
-    - Creates a moving water effect
-    - Creates a moving ship effect
-    - Transitions to the Deck Screen, by zooming in (Screen2)
-*/
-// Call the testData function when the window loads
-window.onload = (event) => {
-  // Fetch data.json file
-  fetch("../../data/screen3data.json")
-    .then((response) => response.json())
-    .then((data) => {
-      //Gathers and stores html elements by id
-      let leftPortrait = document.getElementById("leftCharacterPortrait");
-      let rightPortrait = document.getElementById("rightCharacterPortrait");
-      let backButton = document.getElementById("backButton");
-      let frontButton = document.getElementById("frontButton");
-      let dialogueText = document.getElementById("dialogueText");
-      let dialogueContainer = document.getElementById("dialogueContainer");
+export class Screen3 extends Overlay {
+  constructor(sceneObj) {
+    let shouldTick = false;
+    let isHTML = true;
+    let generateIcon = true;
+    super(shouldTick, isHTML, sceneObj, generateIcon);
 
-      //Other variables necessary for dialogue functionality
-      let index = 0;
-      let scrollIndex = 0; //Character index of scroll
-      let scrollTimer; //Time between characters in text scroll
-      let scrollSpeed = 50;
+    console.log(this.overlayTicker);
+  } // end constructor
 
-      displayDialogue();
+  InitIcon(app, data) {
+    // set settings for Icon
+    super.InitIcon(
+      app,
+      data,
+      ["/assets/images/journal.png"], // the image array to load to animated sprite
+      app.screen.width * 0.9, // x loc of the icon
+      app.screen.height * 0.1, // the y loc of the icon
+      app.screen.height * 0.1, // the width of the icon
+      app.screen.height * 0.1 // the height of the icon
+    ); // end super.InitIcon
+  } // end InitIcon
 
-      backButton.onclick = function (event) {
-        event.stopPropagation();
-        if (index > 0) {
-          index--;
-          displayDialogue();
-        }
-      };
+  Display(app, data, ticker) {
+    super.Display(app, data, ticker);
+    console.log("Display Called");
 
-      // Waits for user to click on screen
-      dialogueContainer.addEventListener("click", function onClick() {
-        displayDialogue();
-      });
+    this.DisplayHTML(["dialogueContainer"], null);
+    //Gathers and stores html elements by id
+    //Class References
+    this.dialogueText = document.getElementById("dialogueText");
+    let dialogueContainer = document.getElementById("dialogueContainer");
+    let backButton = document.getElementById("backButton");
 
-      function displayDialogue() {
-        //if index is less than dialogue array length
-        if (index < data.dialogue.length) {
-          //Sets portraits to dialogue portraits and clears dialogue box
-          displayPortrait();
-          dialogueText.innerHTML = "";
+    //Class variables
+    this.index = 0;
+    this.scrollIndex = 0; //Character index of scroll
+    this.scrollTimer; //Time between characters in text scroll
+    this.scrollSpeed = 50;
 
-          if (index == data.dialogue.length - 1) {
-            frontButton.src = "/assets/images/ui/closeBtn.png";
-          } else {
-            frontButton.src = "/assets/images/ui/ArrowIcon.png";
-          }
+    this.displayDialogue(data, app);
 
-          //if the does already exist
-          if (scrollTimer != null) {
-            skipDialogue();
-          } else {
-            //Sets scrollTimer to scrollingDialogue function for scrollSpeed
-            scrollTimer = setTimeout(scrollingDialogue, scrollSpeed);
-          }
-        } else {
-          location.reload();
-        }
+    backButton.onclick = () => {
+      event.stopPropagation();
+      if (this.index > 0) {
+        this.index--;
+        this.displayDialogue(data, app);
       }
+    };
 
-      function displayPortrait() {
-        //sets leftPortrait's portraits and offsets bottom
-        leftPortrait.src = data.dialogue[index].leftPortrait;
-        leftPortrait.style.bottom = data.dialogue[index].leftOffset + "px";
-        //sets rightPortrait's portraits and offsets bottom
-        rightPortrait.src = data.dialogue[index].rightPortrait;
-        rightPortrait.style.bottom = data.dialogue[index].rightOffset + "px";
-
-        //if isLeftTalking is true
-        if (data.dialogue[index].isLeftTalking) {
-          //Enlarge and brighten left portrait, while shrink and darken right
-          leftPortrait.style.maxWidth = "20%";
-          leftPortrait.style.filter = "brightness(100%)";
-          rightPortrait.style.maxWidth = "17.5%";
-          rightPortrait.style.filter = "brightness(50%)";
-        } else {
-          //Enlarge and brighten right portrait, while shrink and darken left
-          leftPortrait.style.maxWidth = "17.5%";
-          leftPortrait.style.filter = "brightness(50%)";
-          rightPortrait.style.maxWidth = "20%";
-          rightPortrait.style.filter = "brightness(100%)";
-        }
-      }
-
-      function skipDialogue() {
-        //Clears scrollTimer, set it null, and sets scrollIndex to 0
-        clearTimeout(scrollTimer);
-        scrollTimer = null;
-        scrollIndex = 0;
-        //Displays dialogue text without scrolling
-        dialogueText.innerHTML = data.dialogue[index].text;
-        index++;
-      }
-
-      function scrollingDialogue() {
-        //if scrollIndex is less than the dialogue text's length
-        if (scrollIndex < data.dialogue[index].text.length) {
-          //Display scrolling text character in dialogue box at scrollIndex
-          dialogueText.innerHTML +=
-            data.dialogue[index].text.charAt(scrollIndex);
-          scrollIndex++;
-          scrollTimer = setTimeout(scrollingDialogue, scrollSpeed); //Reload scrollingDialogue function and stores it in scrollTimer
-        } else {
-          //Sets scrollIndex null, and sets scrollIndex to 0
-          scrollTimer = null;
-          scrollIndex = 0;
-          index++;
-        }
-      }
-
-      function killSwitch() {
-        clearTimeout(scrollTimer);
-        scrollTimer = null;
-      }
+    // Waits for user to click on screen
+    dialogueContainer.addEventListener("click", () => {
+      this.displayDialogue(data, app);
     });
-}; // end window.onload
+  } // end Display
+
+  Hide(app) {
+    super.Hide(app);
+    this.HideHTML(["dialogueContainer"], null);
+  } // end Hide
+
+  Tick() {
+    super.Tick();
+    console.log("Tick Called");
+  } // end Tick
+
+  displayDialogue(data, app) {
+    let frontButton = document.getElementById("frontButton");
+
+    //if index is less than dialogue array length
+    if (this.index < data.dialogue.length) {
+      //Sets portraits to dialogue portraits and clears dialogue box
+      this.displayPortrait(data);
+      this.dialogueText.innerHTML = "";
+
+      if (this.index == data.dialogue.length - 1) {
+        frontButton.src = "/assets/images/ui/closeBtn.png";
+      } else {
+        frontButton.src = "/assets/images/ui/ArrowIcon.png";
+      }
+
+      //if the does already exist
+      if (this.scrollTimer != null) {
+        this.skipDialogue(data);
+      } else {
+        //Sets scrollTimer to scrollingDialogue function for scrollSpeed
+        this.scrollTimer = setTimeout(() => {
+          this.scrollingDialogue(data);
+        }, this.scrollSpeed);
+      }
+    } else {
+      this.Hide(app);
+    }
+  }
+
+  displayPortrait(data) {
+    //Gets left and right portraits from document
+    let leftPortrait = document.getElementById("leftCharacterPortrait");
+    let rightPortrait = document.getElementById("rightCharacterPortrait");
+
+    //sets leftPortrait's portraits and offsets bottom
+    leftPortrait.src = data.dialogue[this.index].leftPortrait;
+    leftPortrait.style.bottom = data.dialogue[this.index].leftOffset + "px";
+    //sets rightPortrait's portraits and offsets bottom
+    rightPortrait.src = data.dialogue[this.index].rightPortrait;
+    rightPortrait.style.bottom = data.dialogue[this.index].rightOffset + "px";
+
+    //if isLeftTalking is true
+    if (data.dialogue[this.index].isLeftTalking) {
+      //Enlarge and brighten left portrait, while shrink and darken right
+      leftPortrait.style.maxWidth = "20%";
+      leftPortrait.style.filter = "brightness(100%)";
+      rightPortrait.style.maxWidth = "17.5%";
+      rightPortrait.style.filter = "brightness(50%)";
+    } else {
+      //Enlarge and brighten right portrait, while shrink and darken left
+      leftPortrait.style.maxWidth = "17.5%";
+      leftPortrait.style.filter = "brightness(50%)";
+      rightPortrait.style.maxWidth = "20%";
+      rightPortrait.style.filter = "brightness(100%)";
+    }
+  }
+
+  skipDialogue(data) {
+    //Clears scrollTimer, set it null, and sets scrollIndex to 0
+    clearTimeout(this.scrollTimer);
+    this.scrollTimer = null;
+    this.scrollIndex = 0;
+    //Displays dialogue text without scrolling
+    this.dialogueText.innerHTML = data.dialogue[this.index].text;
+    this.index++;
+  }
+
+  scrollingDialogue(data) {
+    //if scrollIndex is less than the dialogue text's length
+    if (this.scrollIndex < data.dialogue[this.index].text.length) {
+      //Display scrolling text character in dialogue box at scrollIndex
+      this.dialogueText.innerHTML += data.dialogue[this.index].text.charAt(
+        this.scrollIndex
+      );
+      this.scrollIndex++;
+      this.scrollTimer = setTimeout(() => {
+        this.scrollingDialogue(data);
+      }, this.scrollSpeed); //Reload scrollingDialogue function and stores it in scrollTimer
+    } else {
+      //Sets scrollIndex null, and sets scrollIndex to 0
+      this.scrollTimer = null;
+      this.scrollIndex = 0;
+      this.index++;
+    }
+  }
+} // end DemoOverlay Class
--- app/StartScreen/StartScreen.js
+++ app/StartScreen/StartScreen.js
@@ -1,79 +1,77 @@
 import { Scene } from "../core/classes/Scene.js";
 
 export class StartScreen extends Scene {
-    constructor() {
-        let shouldTick = false;
-        let isHMTL = true;
-        super(shouldTick, isHMTL);
+  constructor() {
+    let shouldTick = false;
+    let isHMTL = true;
+    super(shouldTick, isHMTL);
 
-        // init class variables
-        this.splash = document.getElementById("splash");
-        this.htmlApp = document.getElementById("app");
-        this.gameContainer = document.getElementById("gameContainer");
-        this.bottomLinks = document.getElementById("bottomLinks");
-        this.music = new Audio("/assets/audio/SFXmusic.mp3");
+    // init class variables
+    this.splash = document.getElementById("splash");
+    this.htmlApp = document.getElementById("app");
+    this.gameContainer = document.getElementById("gameContainer");
+    this.bottomLinks = document.getElementById("bottomLinks");
+    this.music = new Audio("/assets/audio/SFXmusic.mp3");
 
-        // Hide Elements - using the HTML ids
-        this.elementIds = [
-            "settingsContainer",
-            "mapContainer",
-            "scoreContainer",
-            "journalContainer"
-        ]; // end elementIds
-        super.HideHTML(this.elementIds, null);
-    } // end constructor
+    // Hide Elements - using the HTML ids
+    this.elementIds = [
+      "settingsContainer",
+      "mapContainer",
+      "scoreContainer",
+      "journalContainer",
+      "dialogueContainer",
+    ]; // end elementIds
+    super.HideHTML(this.elementIds, null);
+  } // end constructor
 
+  // Called when the Scene is set to run. Starts the Scene.
+  Start(app, data) {
+    // call parent class's Start function -> performs important actions!
+    super.Start(app, data);
 
-    // Called when the Scene is set to run. Starts the Scene.
-    Start(app, data) {
-        // call parent class's Start function -> performs important actions!
-        super.Start(app, data);
+    // set what is displayed
+    super.HideHTML(this.elementIds, null);
+    this.htmlApp.style.display = "block";
+    this.splash.style.display = "block";
+    this.gameContainer.style.display = "block";
+    this.bottomLinks.style.display = "block";
 
-        // set what is displayed
-        super.HideHTML(this.elementIds, null);
-        this.htmlApp.style.display = "block";
-        this.splash.style.display = "block";
-        this.gameContainer.style.display = "block";
-        this.bottomLinks.style.display = "block";
+    this.splash.style.opacity = 1;
 
-        this.splash.style.opacity = 1;
-
-        // set splash to disappear
-        this.splash.addEventListener("click", () => {
-            //Plays background music
-            this.music.volume = 0.4;
-            //music.play();
-            this.splash.style.opacity = 0;
-            setTimeout(() => {
-              this.splash.style.display = "none";
-            }, 610);
-        });
-
-        // set start button to end screen
-        document.getElementById("start").onclick = () => {
-            this.isFinished = true;
-            console.log("Finished Start Screen");
-        } // end getElementById
-    } // end Start
-
-
-    // Called when the Scene has been set to terminate.
-    OnEnd(app) {
-        // call parent class's OnEnd function -> performs important actions!
-        super.OnEnd(app);
-
-        // hide all elements
+    // set splash to disappear
+    this.splash.addEventListener("click", () => {
+      //Plays background music
+      this.music.volume = 0.4;
+      //music.play();
+      this.splash.style.opacity = 0;
+      setTimeout(() => {
         this.splash.style.display = "none";
-        this.htmlApp.style.display = "none";
-        this.gameContainer.style.display = "none";
-        this.bottomLinks.style.display = "none";
-        super.HideHTML(this.elementIds, null);
-    } // end OnEnd
+      }, 610);
+    });
 
+    // set start button to end screen
+    document.getElementById("start").onclick = () => {
+      this.isFinished = true;
+      console.log("Finished Start Screen");
+    }; // end getElementById
+  } // end Start
 
-    // Ticks continuously while the screen is running if 'this.shouldTick' has been set to 'true'.
-    Tick(app, data) {
-        // call parent class's Tick function -> performs important actions... (if active)
-        super.Tick(app, data);
-    } // end Tick
+  // Called when the Scene has been set to terminate.
+  OnEnd(app) {
+    // call parent class's OnEnd function -> performs important actions!
+    super.OnEnd(app);
+
+    // hide all elements
+    this.splash.style.display = "none";
+    this.htmlApp.style.display = "none";
+    this.gameContainer.style.display = "none";
+    this.bottomLinks.style.display = "none";
+    super.HideHTML(this.elementIds, null);
+  } // end OnEnd
+
+  // Ticks continuously while the screen is running if 'this.shouldTick' has been set to 'true'.
+  Tick(app, data) {
+    // call parent class's Tick function -> performs important actions... (if active)
+    super.Tick(app, data);
+  } // end Tick
 } // end Game Scene Class
