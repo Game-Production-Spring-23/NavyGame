c61ed84a7f916b2f4d60d71faa5341d729f114c3
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-03-27 18:40:01 -0400

    added stylesheet param to loadNewHTMLFile function

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ import * as mainMenuScene from './scenes/00-main-menu/script.js';
 
 // loads next screen using an html file (given a path) 
 // and a function to call when the page is loaded
-loadNewHTMLFile('./scenes/00-main-menu/Main-Menu-Scene.html', mainMenuScene.mainMenu);
+loadNewHTMLFile('/scenes/00-main-menu/Main-Menu-Scene.html',
+                '/scenes/00-main-menu/style.css',
+                mainMenuScene.mainMenu);
--- scenes/01-intro-boat-scene/boat-scene.js
+++ scenes/01-intro-boat-scene/boat-scene.js
@@ -5,6 +5,8 @@ import { mainMenu } from '/scenes/00-main-menu/script.js';
 
 export function boatScene() {
     setTimeout(() => {
-        loadNewHTMLFile("/scenes/00-main-menu/Main-Menu-Scene.html", mainMenu);
-    }, 1000);
+        loadNewHTMLFile("/scenes/00-main-menu/Main-Menu-Scene.html", 
+                        "/scenes/00-main-menu/style.css",
+                        mainMenu);
+    }, 24*1000);
 } // end boatScene
