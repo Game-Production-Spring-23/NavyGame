30275e615a129175ef64a03efd651cf162b967f1
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-03-28 19:30:21 -0400

    fixed video length duration for boat scene

--- scenes/01-intro-boat-scene/boat-scene.js
+++ scenes/01-intro-boat-scene/boat-scene.js
@@ -4,9 +4,12 @@ import { loadNewHTMLFile } from '/lib.js';
 import { mainMenu } from '/scenes/00-main-menu/script.js';
 
 export function boatScene() {
-    setTimeout(() => {
-        loadNewHTMLFile("/scenes/00-main-menu/Main-Menu-Scene.html", 
-                        "/scenes/00-main-menu/style.css",
-                        mainMenu);
-    }, 24*1000);
+    let video = document.getElementById("screen_1_video");
+    video.onloadedmetadata = () => {
+        setTimeout(() => {
+            loadNewHTMLFile("/scenes/00-main-menu/Main-Menu-Scene.html", 
+                            "/scenes/00-main-menu/style.css",
+                            mainMenu);
+        }, video.duration * 1000); // end setTimeout
+    } // end onloadedmetatdata
 } // end boatScene
