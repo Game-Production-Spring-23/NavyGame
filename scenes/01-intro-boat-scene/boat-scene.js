6f5edb88fd18f19cbb0c08d9915ad4ce9ffa9b8c
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-03-29 15:13:14 -0400

    unchained splash screen after scene 01 (boat scene). goes to scene 03 (pipe-minigame)

--- scenes/01-intro-boat-scene/boat-scene.js
+++ scenes/01-intro-boat-scene/boat-scene.js
@@ -1,12 +1,13 @@
 import { loadNewHTMLFile } from "/lib.js";
-import { splashScreen } from "/scenes/01.5-splash-screen/splash-screen.js";
+import { loadScene } from "../03-pipe-minigame/pipe-minigame.js";
+//import { splashScreen } from "/scenes/01.5-splash-screen/splash-screen.js";
 
 export function boatScene() {
   setTimeout(() => {
     loadNewHTMLFile(
-      "/scenes/01.5-splash-screen/splash-screen.html",
-      "/scenes/01.5-splash-screen/style.css",
-      splashScreen
+      "/scenes/03-pipe-minigame/pipemini-game.html",
+      "/scenes/03-pipe-minigame/minigame1styles.css",
+      loadScene
     );
   }, 1000); //24 * 1000);
 }
