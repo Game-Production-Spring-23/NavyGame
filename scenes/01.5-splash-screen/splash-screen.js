c297a06fbef6fec63e8de38d00e1e8716bea272a
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-03-29 15:02:58 -0400

    added comments to splash-screen

--- scenes/01.5-splash-screen/splash-screen.js
+++ scenes/01.5-splash-screen/splash-screen.js
@@ -4,9 +4,9 @@ import { loadScene } from "/scenes/03-pipe-minigame/pipe-minigame.js";
 
 export function splashScreen() {
   playSplashScreen(
-    "/scenes/03-pipe-minigame/pipemini-game.html",
-    "/scenes/03-pipe-minigame/minigame1styles.css",
-    loadScene,
-    3000
+    "/scenes/03-pipe-minigame/pipemini-game.html", // next scene's html
+    "/scenes/03-pipe-minigame/minigame1styles.css", // next scene's css
+    loadScene, // the next scene's init function (called when the html & css have loaded)
+    3000 // miliseconds for the delay before transitioning
   );
 } // end splashScreen
