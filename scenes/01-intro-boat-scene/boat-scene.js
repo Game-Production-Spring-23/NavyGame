cb9b99114e6930f807bbc7e41696b417a132211b
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-03-29 16:38:52 -0400

    set boat scene to play in entirety

--- scenes/01-intro-boat-scene/boat-scene.js
+++ scenes/01-intro-boat-scene/boat-scene.js
@@ -1,13 +1,13 @@
-import { loadNewHTMLFile } from "/lib.js";
+//import { loadNewHTMLFile } from "/lib.js";
+import { loadHTMLOnVideoEnd } from "/lib.js";
 import { loadScene } from "../03-pipe-minigame/pipe-minigame.js";
 //import { splashScreen } from "/scenes/01.5-splash-screen/splash-screen.js";
 
 export function boatScene() {
-  setTimeout(() => {
-    loadNewHTMLFile(
-      "/scenes/03-pipe-minigame/pipemini-game.html",
-      "/scenes/03-pipe-minigame/minigame1styles.css",
-      loadScene
-    );
-  }, 1000); //24 * 1000);
-}
+  loadHTMLOnVideoEnd(
+    "/scenes/03-pipe-minigame/pipemini-game.html",
+    "/scenes/03-pipe-minigame/minigame1styles.css",
+    loadScene,
+    "screen_1_video"
+  );
+} // end boatScene
