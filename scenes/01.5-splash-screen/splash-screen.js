a443b0ae41377861409b29fcc1e2baec06117499
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-04-03 18:51:25 -0400

    added splash screen back for testing

--- scenes/01.5-splash-screen/splash-screen.js
+++ scenes/01.5-splash-screen/splash-screen.js
@@ -1,12 +1,12 @@
 // Imports
 import { playSplashScreen } from "../../lib.js";
-import { loadScene } from "/scenes/03-pipe-minigame/pipe-minigame.js";
+import { loadScene2 } from "/scenes/02-deck-explore/scene2.js";
 
 export function splashScreen() {
   playSplashScreen(
-    "/scenes/03-pipe-minigame/pipemini-game.html", // next scene's html
-    "/scenes/03-pipe-minigame/minigame1styles.css", // next scene's css
-    loadScene, // the next scene's init function (called when the html & css have loaded)
+    "/scenes/02-deck-explore/index.html", // next scene's html
+    "/scenes/02-deck-explore/styles.css", // next scene's css
+    loadScene2, // the next scene's init function (called when the html & css have loaded)
     3000 // miliseconds for the delay before transitioning
   );
 } // end splashScreen
