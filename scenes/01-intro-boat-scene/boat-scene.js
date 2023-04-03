dad3c1804c324d1f94e1b057053b79f3b7b5708f
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-04-03 18:51:25 -0400

    added splash screen back for testing

--- scenes/01-intro-boat-scene/boat-scene.js
+++ scenes/01-intro-boat-scene/boat-scene.js
@@ -1,13 +1,13 @@
 //import { loadNewHTMLFile } from "/lib.js";
 import { loadHTMLOnVideoEnd } from "/lib.js";
-import { loadScene2 } from "/scenes/02-deck-explore/scene2.js";
-//import { splashScreen } from "/scenes/01.5-splash-screen/splash-screen.js";
+//import { loadScene2 } from "/scenes/02-deck-explore/scene2.js";
+import { splashScreen } from "/scenes/01.5-splash-screen/splash-screen.js";
 
 export function boatScene() {
   loadHTMLOnVideoEnd(
-    "/scenes/02-deck-explore/index.html",
-    "/scenes/02-deck-explore/styles.css",
-    loadScene2,
+    "/scenes/01.5-splash-screen/splash-screen.html",
+    "/scenes/01.5-splash-screen/style.css",
+    splashScreen,
     "screen_1_video"
   );
 } // end boatScene
