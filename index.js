c6511cab00af2283c5c3796c1bc7159073163ce5
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-04-10 17:21:09 -0400

    added ui to main index files

--- index.js
+++ index.js
@@ -14,3 +14,58 @@ loadNewHTMLFileIndex(
 );
 
 export { transition };
+
+
+let settingModal = document.getElementById("settingContainer");
+let mainContainer = document.getElementById("app");
+let journalScreen = document.getElementById("journalContainer");
+let mapScreen = document.getElementById("mapContainer");
+let music = document.getElementById("audio");
+let settingsContainer = document.getElementById("settingsContainer");
+
+let mapStatus = false;
+let journalStatus = false;
+let volume = true;
+
+journalScreen.style.display = "none";
+mapScreen.style.display = "none";
+settingsContainer.style.display = "none";
+
+function toggleVolume() {
+    if (volume == true) {
+      music.volume = 0;
+      volume = false;
+      let volumeIcon = document.getElementById("volumeIcon");
+    } else {
+      music.volume = 0.4;
+      volume = true;
+    }
+  }
+
+ function toggleMap(){
+    if (mapStatus == false) {
+      mapScreen.style.display="block";
+      console.log("map on");
+      mapStatus = true;
+    } else {
+      mapScreen.style.display="none";
+      console.log("map off");
+      mapStatus = false;
+    }
+  }
+
+  //shows the player hournal when clicked
+  function toggleJournal(){
+    if (journalScreen.style.display == "none") {
+      journalScreen.style.display="block";
+      console.log("journal on");
+    } else {
+      journalScreen.style.display="none";
+      console.log("journal off");
+    }
+  }
+
+
+document.getElementById("mapOverlay").onclick = () => { toggleMap(); };
+document.getElementById("journalOverlay").onclick = () => { toggleJournal(); };
+document.getElementById("volumeIcon").onclick = () => { toggleVolume(); };
