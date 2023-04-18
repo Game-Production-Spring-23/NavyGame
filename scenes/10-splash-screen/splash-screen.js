ee6e9e46da6af8544ab3f7271689ebfffba29b4e
Edwin Sanchez <edwinsanchez2003@gmail.com>
2023-04-17 21:21:33 -0400

    added mini game 4, but has issues

--- scenes/10-splash-screen/splash-screen.js
+++ scenes/10-splash-screen/splash-screen.js
@@ -1,18 +1,18 @@
 // Imports
 import { playSplashScreen, devSkip } from "../../lib.js";
-import { loadScene2 } from "/scenes/02-deck-explore/scene2.js";
+import { loadScene11 } from "../11-explore/scene11.js";
 
 export function splashScreen() {
   devSkip(
-    "/scenes/02-deck-explore/index.html",
-    "/scenes/02-deck-explore/styles.css",
-    loadScene2
+    "/scenes/11-explore/index.html",
+    "/scenes/11-explore/styles.css",
+    loadScene11
   )
 
   playSplashScreen(
     "/scenes/02-deck-explore/index.html", // next scene's html
     "/scenes/02-deck-explore/styles.css", // next scene's css
-    loadScene2, // the next scene's init function (called when the html & css have loaded)
+    loadScene11, // the next scene's init function (called when the html & css have loaded)
     4000 // miliseconds for the delay before transitioning
   );
 } // end splashScreen
