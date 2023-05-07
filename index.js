3cffe14ccd081784f4f4b8482eadeb3795a97c22
Evie <61148035+eviemayhem@users.noreply.github.com>
2023-05-07 12:54:17 -0400

    Rank system now online

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ let mapImage = document.getElementById("mapModal");
 journalScreen.style.display = "none";
 mapScreen.style.display = "none";
 settingsContainer.style.display = "none";
+let charRank = document.getElementById("charRank");
 
 let uiOverlay = document.getElementById("uiOverlay");
 
@@ -51,92 +52,110 @@ function getMap(){
   //get page from local storage
   let page=localStorage.getItem("page");
 
-  //switch statement to get map image
+  //switch statement to get map image, journal content and rank
   switch (page) {
     case "0":
-      //apply .mapBlank class to mapModal
+     
+      charRank.classList.add("rank1");
       mapImage.classList.add("mapBlank");
       console.log("screen 0 starting screen");
       break;
     case "1":
+      charRank.classList.add("rank1");
       mapImage.classList.add("mapBlank");
       console.log("screen 1 starting screen");
       break;
     case "2":
+      charRank.classList.add("rank1");
       mapImage.classList.add("mapBlank");
       console.log("screen2 = boatscreen");
       break;
     case "3":
+      charRank.classList.add("rank1");
       mapImage.classList.add("mapBlank");
       console.log("screen 3 = boat Explore 1");
       break;
 
     case "4":
+    charRank.classList.add("rank1");
     mapImage.classList.add("map1");
     console.log("screen 4 = boiler room");  
     break;
 
     case "5":
+    charRank.classList.add("rank2");
     mapImage.classList.add("map1");
     console.log("screen 5 = ship splash");
     break;
 
     case "6":
+    charRank.classList.add("rank2");
     mapImage.classList.add("map1");
     console.log("screen 6 = ship crash splash ");
     break;
 
     case "7":
+    charRank.classList.add("rank2");
     mapImage.classList.add("map2");
     console.log("screen 7 = beach explore1 ");
     break;
 
     case "8":
+    charRank.classList.add("rank2");
     mapImage.classList.add("map2");
     console.log("screen 8 = minigame2");
     break;
 
     case "9":
+    charRank.classList.add("rank3");
     mapImage.classList.add("map2");
     console.log("screen 9 = fixing splash");
     break;
 
     case "10":
+    charRank.classList.add("rank3");
     mapImage.classList.remove("map2");
     mapImage.classList.add("map3");
     console.log("screen 10 = island splash ");
     break;
 
     case "11":
+    charRank.classList.add("rank3");
     mapImage.classList.add("map3");
     console.log("screen 11 = jungle explore1");
     break;
 
     case "12":
+    charRank.classList.add("rank3");
     mapImage.classList.add("map4");
     console.log("screen 12 = blacksmith minigame ");
     break;
 
     case "13":
+    charRank.classList.add("rank4");
     mapImage.classList.add("map4");
     console.log("screen 13 = splash");
 
     case "14":
+    charRank.classList.add("rank4");
     mapImage.classList.add("map6");
     console.log("screen 14 = beach explore 2");
     break;
 
     case "15":
+    charRank.classList.add("rank4");
     mapImage.classList.add("map6");
     console.log("screen 15 = probability game ");
     break;
 
     case "16":
+    charRank.classList.add("rank5");
     mapImage.classList.add("map2");
     console.log("screen 16 = resources minigame ");
     break;
 
     case "17":
+    charRank.classList.add("rank6");
     mapImage.classList.add("map2");
     console.log("screen 17 = ending boat explore");
     break;
@@ -233,6 +252,7 @@ document.getElementById("volumeIcon").onclick = () => {
   toggleVolume();
 };
 document.getElementById("uiTab").onclick = () => {
+  getMap();
   if (uiOpen) {
     closeUI();
   } else {
